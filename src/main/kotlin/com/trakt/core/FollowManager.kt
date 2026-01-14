dccd7f0cdea4f66571064e0ab672391b8fd48d87
Bill Hellier <bill.hellier@gmail.com>
2026-01-14 00:07:46 -0600

    followmanager owner/target ordering was wrong

--- src/main/kotlin/com/trakt/core/FollowManager.kt
+++ src/main/kotlin/com/trakt/core/FollowManager.kt
@@ -4,12 +4,15 @@ import com.trakt.data.UserRepository
 import dev.kord.core.Kord
 import dev.kord.core.behavior.MemberBehavior
 import dev.kord.core.behavior.channel.MessageChannelBehavior
+import dev.kord.core.behavior.channel.createMessage
 import dev.kord.core.event.message.MessageCreateEvent
+import dev.kord.rest.builder.message.addFile
 import kotlin.time.ComparableTimeMark
 import kotlin.time.Duration.Companion.seconds
 import kotlin.time.TimeSource
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
+import java.nio.file.Path
 
 class FollowManager(
     private val kord: Kord,
@@ -45,7 +48,7 @@ class FollowManager(
     }
   }
 
-  fun handleFollow(target: ULong, owner: ULong, timeout: Int) {
+  fun handleFollow(owner: ULong, target: ULong, timeout: Int) {
     activeFollows.getOrPut(target) { mutableMapOf() }[owner] =
         Pair(timeout, timeSource.markNow() - timeout.seconds)
   }
@@ -63,5 +66,8 @@ class FollowManager(
     val username = MemberBehavior(config.guild.snowflake, target.snowflake, kord).asUser().username
     MessageChannelBehavior(config.followChannel.snowflake, kord)
         .createMessage("$mention Your follow for $username triggered here: $url")
+    MessageChannelBehavior(config.followChannel.snowflake, kord).createMessage {
+      addFile(Path.of(""))
+    }
   }
 }
